9c2db59a9b0d731614074baf3784d84250554eee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 15:10:59 +0000

    Deploy Pozas91/vue-movies to github.com/Pozas91/vue-movies.git:gh-pages

---
+++ precache-manifest.7990635fa8cb1d1b7f93047a3d019b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17afe4d7a1fa230cea95",
+    "url": "/css/chunk-vendors.ad699ded.css"
+  },
+  {
+    "revision": "ec4b00c1407ca0d69c11058735c7ccc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c41b3735930765fccf0",
+    "url": "/js/app.bbb910c2.js"
+  },
+  {
+    "revision": "17afe4d7a1fa230cea95",
+    "url": "/js/chunk-vendors.49b901fb.js"
+  },
+  {
+    "revision": "37949ed4e3b36178d936a08eab27c73f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
